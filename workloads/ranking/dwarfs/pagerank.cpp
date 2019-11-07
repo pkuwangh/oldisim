16a09e009c7256d4f425ed8040fb525e7eb27adb
Carlos L. Torres <cltorres@fb.com>
2019-11-07 15:35:05 -0800

    Add gapbs as submodule

--- workloads/ranking/dwarfs/pagerank.cpp
+++ workloads/ranking/dwarfs/pagerank.cpp
@@ -1,16 +1,27 @@
-// Copyright (c) 2019-present, Facebook, Inc. and its affiliates.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright (c) 2015, The Regents of the University of California (Regents).
+// All Rights Reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the Regents nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL REGENTS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "pagerank.h"
 #include <string>
@@ -25,9 +36,12 @@ PageRank::PageRank(int scale, int degrees) : scale_(scale), degrees_(degrees) {
   char *argv[4] = {(char *)"pr", (char *)"-u", (char *)std::to_string(scale_).c_str()};
   CLPageRank cli{3, argv, "pagerank", 1e-4, 20};
   Builder b{cli};
-  graph_ = std::move(b.MakeGraph(scale_, degrees_, true));
+  graph_ = std::move(b.MakeGraph());
 }
 
+/** PageRank implementation taken from
+* http://gap.cs.berkeley.edu/benchmark.html
+*/
 pvector<float> PageRank::rank(int max_iters, double epsilon) {
   const float init_score = 1.0f / graph_.num_nodes();
   const float base_score = (1.0f - kDamp) / graph_.num_nodes();
